78110e11a2bbaf3b5aafe78854027593bd632d23
Mario Pabon <me@mariopabon.com>
2016-10-13 15:03:32 -0400

    Serve static assets

--- server/pup.js
+++ server/pup.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const path = require('path');
+const {renderFile} = require('swig');
+
 const app = express();
 
+// Use swig for view engine
+app.engine('html', renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+// Enable view cache
+app.enable('view cache');
+
+// Serve static assets
+app.use(
+  express.static(
+    path.join(__dirname, '..', 'dist')
+  )
+);
+
 app.get('/', (req, res) => {
-  res.send('hai');
+  res.render('index');
 });
 
 module.exports = app;
